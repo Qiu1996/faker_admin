9374f57695e9a5b1703daed21a3d67ab3000e0a7
Qiu1996 <betty52332001@gmail.com>
2025-10-31 23:00:49 +0800

    feat(fe): 初始化 Vue 3 專案並串接後端 API

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+fetch("https://fakeradmin.zeabur.app/")
+  .then(res => res.json())
+  .then(data => console.log(data))
+</script>
+
+<template>
+<h1>Hello</h1>
+</template>
+
+<style scoped>
+
+</style>

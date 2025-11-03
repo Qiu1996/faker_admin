cb57ed26242ade0596399c448c74bd0db503868d
Qiu1996 <betty52332001@gmail.com>
2025-11-03 20:18:19 +0800

    feat(fe): 實作全欄位排序功能

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,21 +3,42 @@ import { ref, computed } from "vue"
 import OrderTable from "./components/OrderTable.vue";
 import Pagination from "./components/Pagination.vue"
 
-const orders = ref([]);
+const orderList = ref([]);
 const currentPage = ref(1);
 const pageSize = ref(10);
 
 const fetchOrdersData = async () => {
-  const res = await fetch("https://fakeradmin.zeabur.app/");
-  orders.value = await res.json();
+  const res = await fetch("http://localhost:8000/");
+  orderList.value = await res.json();
 };
 fetchOrdersData();
 
 const displayedOrders = computed(() => {
   const start = (currentPage.value - 1) * pageSize.value
   const end = start + pageSize.value
-  return orders.value.slice(start, end)
+  return orderList.value.slice(start, end)
 })
+
+const handleSort = ({ prop, order }) => {
+  if (!order) {
+    orderList.value.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    return;
+  }
+
+  const direction = order === "ascending" ? -1 : 1;
+
+  const comparators = {
+    order_number: (item1, item2) => item1.localeCompare(item2),
+    customer_name: (item1, item2) => item1.localeCompare(item2, "zh-TW"),
+    amount: (item1, item2) => item1 - item2,
+    status: (item1, item2) => item1.localeCompare(item2),
+    created_at: (item1, item2) => new Date(item1) - new Date(item2)
+  };
+
+  orderList.value.sort((item1, item2) =>
+    comparators[prop](item1[prop], item2[prop]) * direction
+  );
+}
 
 </script>
 
@@ -26,6 +47,9 @@ const displayedOrders = computed(() => {
 <Pagination
   v-model:current-page="currentPage"
   :page-size=pageSize
-  :total=orders.length />
-<OrderTable :orders=displayedOrders />
+  :total=orderList.length />
+<OrderTable
+  :orders=displayedOrders
+  @sort-change="handleSort"
+  />
 </template>
--- frontend/src/components/OrderTable.vue
+++ frontend/src/components/OrderTable.vue
@@ -1,14 +1,17 @@
 <script setup>
 const props = defineProps(["orders"]);
+const emit = defineEmits(["sort-change"]);
 
 </script>
 
 <template>
-<el-table :data="orders" style="width: 100%">
-  <el-table-column prop="order_number" label="訂單編號" />
-   <el-table-column prop="customer_name" label="客戶姓名" />
-   <el-table-column prop="amount" label="訂單金額" />
-   <el-table-column prop="status" label="付款狀態" />
-   <el-table-column prop="created_at" label="訂單建立時間" />
+<el-table
+  @sort-change="$emit('sort-change', $event)"
+  :data="orders" style="width: 100%">
+  <el-table-column sortable="custom" prop="order_number" label="訂單編號" />
+  <el-table-column sortable="custom" prop="customer_name" label="客戶姓名" />
+  <el-table-column sortable="custom" prop="amount" label="訂單金額" />
+  <el-table-column sortable="custom" prop="status" label="付款狀態" />
+  <el-table-column sortable="custom" prop="created_at" label="訂單建立時間" />
 </el-table>
 </template>
